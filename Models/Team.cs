b2cb7090d465556b2087d26d3ffa7e0286fe6f79
w4v3 <d15tr1cktf0x@g04t0rd1n4ry.local>
2021-10-05 11:18:54 -0700

    initial commit

---
+++ Models/Team.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace Code1st.Models
+{
+    public class Team
+    {
+        [Key]
+        public string TeamName { get; set; }
+        public string City { get; set; }
+        public List<Player> Players {get; set;}
+        
+        public Team()
+        {
+
+        }
+    }
+}
